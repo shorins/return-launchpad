21e536ac7c6e3f986ad1bea9c087a554589045d9
MightyShorin <mighty.shorin@ya.ru>
2025-08-22 01:30:25 +0300

    Initial Commit

---
+++ Return Launchpad/Return_LaunchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Return_LaunchpadApp.swift
+//  Return Launchpad
+//
+//  Created by Сергей Шорин on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Return_LaunchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Return LaunchpadTests/Return_LaunchpadTests.swift
@@ -0,0 +1,17 @@
+//
+//  Return_LaunchpadTests.swift
+//  Return LaunchpadTests
+//
+//  Created by Сергей Шорин on 22.08.2025.
+//
+
+import Testing
+@testable import Return_Launchpad
+
+struct Return_LaunchpadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
